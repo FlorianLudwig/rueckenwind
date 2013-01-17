262b3fa56ac03b14507fc7b6aeaaf5b963fd6607
Florian Ludwig <f.ludwig@greyrook.com>
2013-01-17 13:36:48 +0100

    include data files in setup

--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ setup(
     install_requires=['tornado>=2.2.1', 'jinja2', 'werkzeug==0.6.2', 'babel'],
     packages=find_packages(exclude=["test"]),
     include_package_data=True,
+    package_data={
+        'rw': ['*.html', '*.css', 'templates/html5']
+    },
     entry_points={
         'console_scripts': [
             'rw = rw.cli:main',
