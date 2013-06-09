d959b5d57395fd5bbbbe96cc4b74d919349e9be4
Florian Ludwig <f.ludwig@greyrook.com>
2013-06-09 19:44:43 +0200

    most basic DBField

--- rw/form.py
+++ rw/form.py
@@ -1,7 +1,11 @@
-from wtforms import TextField
+from . import db
+from wtforms import TextField, validators
 
 
 def DBField(db_field):
     """create WTForm Field from rw.db attribute"""
     # title = db_field.title
-    return TextField('1' ) # db_field.title)
+    validator_list = []
+    if db_field.default is db.NoDefaultValue:
+        validator_list.append(validators.Required())
+    return TextField(db_field.name, validator_list) # db_field.title)
