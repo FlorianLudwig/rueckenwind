ca120ce60fe05a24cb7628a0848c95070a82ae14
Florian Ludwig <f.ludwig@greyrook.com>
2013-09-02 15:07:55 +0200

    bump version number

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ if '--dev' in sys.argv:
 
 setup(
     name="rueckenwind",
-    version="0.0.1" + version_suffix,
+    version="0.2.0" + version_suffix,
     # use fix versions of motor und PyMongo for now, see:
     # https://groups.google.com/forum/?hl=de&fromgroups=#!topic/python-tornado/xEpZ_NU5eDE
     install_requires=['tornado>=3.0.1,<4.0', 'jinja2', 'werkzeug==0.6.2', 'babel', 'mock', 'configobj', 'chardet',
