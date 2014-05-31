9efcb9b2c9f3a22ced05cf35bcd5c84114bfda26
Florian Ludwig <f.ludwig@greyrook.com>
2014-05-31 17:38:42 +0200

    cleanup

--- setup.py
+++ setup.py
@@ -36,8 +36,6 @@ setup(
     url='https://github.com/FlorianLudwig/rueckenwind',
     description='tornado based webframework',
     author='Florian Ludwig',
-    # use fix versions of motor und PyMongo for now, see:
-    # https://groups.google.com/forum/?hl=de&fromgroups=#!topic/python-tornado/xEpZ_NU5eDE
     install_requires=['tornado>=3.0.1,<4.0',
                       'jinja2',
                       'werkzeug==0.6.2',
@@ -46,9 +44,8 @@ setup(
                       'mock',
                       'configobj',
                       'chardet',
-                      'motor==0.1.1',
-                      'PyMongo==2.5.0',
                       'pytz',
+                      'PyYAML>=3.10'
                       ],
     packages=find_packages(exclude=['*.test', '*.test.*']),
     include_package_data=True,
