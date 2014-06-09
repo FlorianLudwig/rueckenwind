d02cf838f929f730af77923e414bc49a6fadf9e0
Florian Ludwig <f.ludwig@greyrook.com>
2014-06-09 19:31:27 +0200

    move old files out of the way

--- test/simple_app/www.py
+++
@@ -1,7 +0,0 @@
-from rw.www import RequestHandler, post
-
-
-class Main(RequestHandler):
-    @post('/')
-    def index(self):
-        self.finish(template='index.html')
--- test/test_widget.py
+++
@@ -1,7 +0,0 @@
-import rw
-
-
-# def test_widget():
-#     www = rw.get_module('rw.test.simple_app', 'www')
-#     print www
-#     assert 'example' in www.widgets
