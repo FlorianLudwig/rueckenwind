af5832714538ef4a778efb1270d42de8c04b0989
Florian Ludwig <f.ludwig@greyrook.com>
2014-06-14 20:09:13 +0200

    rw.template module

---
+++ rw/template.py
@@ -0,0 +1,36 @@
+"""Rueckenwind tempating
+
+Rueckenwind does not contain it's own templating engine
+but is based on Jinja2.
+"""
+import jinja2
+
+
+def create_template_env(pkgs):
+    # template pathes
+    # TODO use jinja2.PackageLoader
+    loaders = [jinja2.PackageLoader(pkg, 'templates') for pkg in pkgs]
+    template_env = jinja2.Environment(
+        loader=jinja2.ChoiceLoader(loaders),
+        extensions=['jinja2.ext.loopcontrols',
+                    'jinja2.ext.i18n',
+        ])
+
+    # some more default functions
+    # template_env.globals['enumerate'] = enumerate
+    # template_env.globals['isinstance'] = isinstance
+    # template_env.globals['len'] = len
+    # # default types
+    # template_env.globals['int'] = int
+    # template_env.globals['str'] = str
+    # template_env.globals['unicode'] = unicode
+    # template_env.globals['list'] = list
+    # template_env.globals['tuple'] = tuple
+    # template_env.globals['dict'] = dict
+    # template_env.globals['set'] = set
+    # template_env.globals['basestring'] = basestring
+    # template_env.globals['urlencode'] = urlencode
+    # filter
+    # template_env.filters['json'] = dumps
+    return template_env
+    # scope['template_env'] = template_env
