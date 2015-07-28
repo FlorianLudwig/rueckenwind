722c54acc3cd60601440c5a6841bd89f4908d5ff
Florian Ludwig <f.ludwig@greyrook.com>
2015-07-28 11:51:12 +0200

    bump to 0.4.2

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ class TestCommand(setuptools.command.test.test):
 
 setup(
     name="rueckenwind",
-    version="0.4.1",
+    version="0.4.2",
     url='https://github.com/FlorianLudwig/rueckenwind',
     description='tornado based webframework',
     author='Florian Ludwig',
