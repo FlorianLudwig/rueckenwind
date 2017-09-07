d20ab98d49075d91a82c435bfd038b66ba18a22d
Florian Ludwig <f.ludwig@greyrook.com>
2017-09-07 21:16:43 +0200

    goodbye tox

--- setup.py
+++ setup.py
@@ -14,39 +14,6 @@ version_suffix = ''
 with open('README.rst') as readme_file:
     readme = readme_file.read()
 
-
-class TestCommand(setuptools.command.test.test):
-    def finalize_options(self):
-        setuptools.command.test.test.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        fails = []
-        from tox.config import parseconfig
-        from tox.session import Session
-
-        config = parseconfig(self.test_args)
-        retcode = Session(config).runcommand()
-        if retcode != 0:
-            fails.append('tox returned errors')
-
-        import pep8
-        style_guide = pep8.StyleGuide(config_file=BASE_PATH + '/.pep8')
-        style_guide.input_dir(BASE_PATH + '/rw')
-        if style_guide.options.report.get_count() != 0:
-            fails.append('pep8 returned errros for rw/')
-
-        style_guide = pep8.StyleGuide(config_file=BASE_PATH + '/.pep8')
-        style_guide.input_dir(BASE_PATH + '/test')
-        if style_guide.options.report.get_count() != 0:
-            fails.append('pep8 returned errros for test/')
-
-        if fails:
-            print('\n'.join(fails))
-            sys.exit(1)
-
-
 setup(
     name="rueckenwind",
     version="0.5.0",
@@ -66,7 +33,7 @@ setup(
                       'future'
                       ],
     extras_requires={
-        'test': ['tox', 'pytest', 'pep8'],
+        'test': ['pytest', 'pep8'],
         'docs': ['sphinx_rtd_theme']
     },
     packages=find_packages(exclude=['*.test', '*.test.*']),
@@ -78,9 +45,6 @@ setup(
         'console_scripts': [
             'rw = rw.cli:main',
         ],
-    },
-    cmdclass={
-        'test': TestCommand
     },
     license="http://www.apache.org/licenses/LICENSE-2.0",
     classifiers=[
